772ffec26b5761c098890c817064cfa5230ac0bb
quinnbl <quinnbl@hawaii.edu>
2021-11-03 16:02:32 -1000

    contacts collection finished in 17 minutes

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Contacts } from '../../api/contact/Contacts';
 
 /* eslint-disable no-console */
 
@@ -14,5 +15,18 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+function addContacts(data) {
+  console.log(`  Adding: ${data.lastName} (${data.owner})`);
+  Contacts.collection.insert(data);
+}
+
+// Initialize the StuffsCollection if empty.
+if (Contacts.collection.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating default Contacts.');
+    Meteor.settings.defaultContacts.map(data => addContacts(data));
   }
 }
